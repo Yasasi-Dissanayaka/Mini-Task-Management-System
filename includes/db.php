04d163199a50daffff9e715562caecd9f237f5a4
Yasasi <yasasianjanadissanayaka@gmail.com>
2026-06-26 11:48:34 +0530

    task management system

---
+++ includes/db.php
@@ -0,0 +1,10 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass = "MySql1111";
+$db = "intern_task_system";
+$conn=mysqli_connect($host,$user,$pass,$db);
+if (!$conn){
+    die("Connection failed" . mysqli_connect_error());
+}
+?>
