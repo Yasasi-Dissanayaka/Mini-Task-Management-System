6f1242cb60e8554fd5d9f936bd0c984ee057bfc9
Yasasi <yasasianjanadissanayaka@gmail.com>
2026-06-29 13:39:06 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -17,7 +17,11 @@ $result = $conn->query("SELECT * FROM tasks ORDER BY id DESC");
     <form action = "actions/add-task.php" method = 'POST'>
 
     <label><B>Title</B></label><br>
-    <input type="text" name="title" placeholder="Title"><br><br>
+    <input 
+        type="text" 
+        name="title" 
+        placeholder="Enter the title" 
+        required><br><br>
 
     <label><B>Description</B></label><br>
     <textarea name="description" placeholder ="Description"></textarea><br><br>
@@ -25,8 +29,8 @@ $result = $conn->query("SELECT * FROM tasks ORDER BY id DESC");
     <label><B>Priority</B></label><br>
     <select name="priority">
         <option value = "Low">Low</option>
-        <option value = "Medium">Low</option>
-        <option value = "High">Low</option>
+        <option value = "Medium">Medium</option>
+        <option value = "High">High</option>
     </select><br><br>
 
     <button type="submit">Add Task</button>
@@ -36,10 +40,10 @@ $result = $conn->query("SELECT * FROM tasks ORDER BY id DESC");
 
     <h2>Task List</h2>
 
-    <table>
+    <table border='1'>
         <tr>
             <th>ID</th>
-            <th>Titl</th>
+            <th>Title</th>
             <th>Description</th>
             <th>Priority</th>
             <th>Created_at</th>
@@ -48,24 +52,25 @@ $result = $conn->query("SELECT * FROM tasks ORDER BY id DESC");
         </tr>
     <?php while($row = $result->fetch_assoc()){?>
         <tr>
-            <td><?php echo $row['id'];?</td>
-            <td><?php echo $row['title'];?</td>
-            <td><?php echo $row['description'];?</td>
-            <td><?php echo $row['priority'];?</td>
-            <td><?php echo $row['created_at'];?</td>
-            <td><?php echo $row['status'];?</td>
-            <td><?php echo $row['actions'];?</td>
+            <td><?php echo $row['id'];?></td>
+            <td><?php echo $row['title'];?></td>
+            <td><?php echo $row['description'];?></td>
+            <td><?php echo $row['priority'];?></td>
+            <td><?php echo $row['created_at'];?></td>
+            <td><?php echo $row['status'];?></td>
+            
         
-        </tr>
-        <td>
-            <a href="actions/update-task.php?id=<?php echo $row['id'];?>">
-                Update status
-            </a>
+        
+            <td>
+                <a href="actions/update-task.php?id=<?php echo $row['id'];?>">
+                    Update status
+                </a>
 
-            <a href="actions/delete-task.php?id=<?php echo $row['id'];?>">
-                Delete
-            </a>
-        </td>
+                <a href="actions/delete-task.php?id=<?php echo $row['id'];?>">
+                    Delete
+                </a>
+            </td>
+        </tr>
 
     <?php}?>
         
